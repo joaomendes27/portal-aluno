5b44766d21e977df650f2a2e21e13ec2bd75232d
guilhermelins7 <guilherme7lins@gmail.com>
2026-01-18 19:52:13 -0300

    feat: Start auth page; Components input and button

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './pages/auth/auth.component';
+import { ButtonComponent } from './shared/components/button/button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { InputComponent } from './shared/components/input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    ButtonComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/src/app/pages/auth/auth.component.ts
+++ Front/src/app/pages/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from 'src/app/shared/components/button/button.component';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
+  email: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
 
+  onSubmit() {
+    console.log('Form submitted:', {
+      email: this.email,
+      password: this.password,
+      rememberMe: this.rememberMe
+    });
+  }
 }
