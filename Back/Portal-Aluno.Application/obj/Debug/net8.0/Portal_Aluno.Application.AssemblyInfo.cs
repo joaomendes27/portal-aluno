087e72efd4d76abb346d381969eac5faa183ea3c
João Mendes <joao.lima@mosten.com>
2026-02-03 10:16:52 -0300

    fix: removendo arquivos duplicados

--- Back/Portal-Aluno.Application/obj/Debug/net8.0/Portal_Aluno.Application.AssemblyInfo.cs
+++ Back/Portal-Aluno.Application/obj/Debug/net8.0/Portal_Aluno.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal_Aluno.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+633eee59dbad1fc40c9232018299eea5f41259ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358d01ae2fb65e68dec9ffe46ce1561df640fcc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal_Aluno.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal_Aluno.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
